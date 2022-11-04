40b3f808b93931ee45c488941ac2e080f61b5383
c2031089 <c2031089@ncl.ac.uk>
2022-11-04 11:25:50 +0000

    added getters and setters to activity class

--- src/Activity.java
+++ src/Activity.java
@@ -1,2 +1,30 @@
 public class Activity {
+    private int numberActivities;
+    private int numberCustomers;
+    private String activityName;
+    private int ticketsAvailable;
+
+    public Activity(String activityName, int ticketsAvailable) {
+        this.activityName = activityName;
+        this.ticketsAvailable = ticketsAvailable;
+    }
+
+    public String getActivityName(){
+        return activityName;
+    }
+
+    public void setActivityName() {
+        this.activityName=activityName;
+    }
+    public int getTicketsAvailable() {
+        return ticketsAvailable;
+    }
+
+    public void setTicketsAvailable() {
+        this.ticketsAvailable=ticketsAvailable;
+    }
+    @Override
+    public String toString() {
+        return ticketsAvailable + " tickets available for " + activityName + ".";
+    }
 }
